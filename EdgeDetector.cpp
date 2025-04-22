7ae5f7f2a88c2a642eb6ed11869cf0b01136c54f
Gurulakshmi SS <gurulakshmisubramaniam@gmail.com>
2025-04-22 19:32:34 +0530

    Add support for reading image

--- EdgeDetector.cpp
+++ EdgeDetector.cpp
@@ -1,5 +1,10 @@
+#include <opencv2/opencv.hpp>
 #include<stdio.h>
+
+using namespace cv;
 int main(){
     printf("hello world");
+    Mat image = imread("input/cat.jpeg", IMREAD_COLOR);
     return 0;
 }
+
