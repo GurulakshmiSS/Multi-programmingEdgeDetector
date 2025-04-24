1d2c4c3aa5d930f4ab5f97d377a86615bf447226
Gurulakshmi SS <gurulakshmisubramaniam@gmail.com>
2025-04-24 13:27:35 +0530

    Add code for making every 100th row black

--- EdgeDetector.cpp
+++ EdgeDetector.cpp
@@ -4,7 +4,19 @@
 using namespace cv;
 int main(){
     Mat image = imread("input/cat.jpeg", IMREAD_GRAYSCALE);
-
+    printf("Image Height: %d\n",image.rows);
+    printf("Image Width: %d\n",image.cols);
+    printf("First Pixel: %d\n",image.data[0]);
+    int n = image.cols * image.rows;
+    int n2 = image.cols * 100;
+    for(int i = n2;i < n;i = i + n2){
+        for(int j = i; j < i+500; j++){
+            image.data[j] = 0;
+            
+        }
+        
+    }
+    
     imwrite("output/cat_Edge.jpg", image);
 
     return 0;
